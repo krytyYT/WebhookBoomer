bfb31898ffdcb492650e7976451adf61e768a415
krytyYT <49837837+krytyYT@users.noreply.github.com>
2020-05-28 10:38:26 +0200

    Add files via upload

---
+++ boomer.py
@@ -0,0 +1,42 @@
+# Created by krytyYT
+# github.com/krytyYT/WebhookBoomer
+import os
+import sys
+import string
+import pyfiglet
+import random
+from discord_webhook import DiscordWebhook
+
+proxy_file = open("proxy.txt", "r")
+proxy_text = proxy_file.readlines()
+
+def proxies():
+    line = random.choice(proxy_text)
+    ip = line.replace('\n', '')
+    if str(ip).startswith('http'):
+        pass
+    else:
+        https = "https://"+ip
+        http = "http://"+ip
+    proxy = {
+        "https":https,
+        "http":http
+    }
+    return proxy
+os.system("cls")
+result = pyfiglet.figlet_format("Webhook Boomer", font = "slant") 
+print(result)
+print("\n         github.com/krytyYT/WebhookBoomer\n\n")
+prox = str(input("    [?] Use proxies (Yes/No): "))
+option = str(input("    [?] Your message: "))
+option2 = str(input("    [?] Your webhook url: "))
+print("\n\n     If you want to stop close program this is easy xD")
+while True:
+    if prox == "yes":
+        webhook = DiscordWebhook(url=option2, content=option, username="Spamming with: github.com/krytyYT/WebhookBoomer", proxies=proxies())
+        response = webhook.execute()
+        print(response)
+    else:
+        webhook = DiscordWebhook(url=option2, content=option, username="Spamming with: github.com/krytyYT/WebhookBoomer")
+        response = webhook.execute()
+        print(response)
